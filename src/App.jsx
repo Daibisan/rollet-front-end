d78afcbbdbf59fa87c71d96cf0864064831f7473
Daibisan <dibizeno@gmail.com>
2025-10-28 22:22:07 +0700

    UI: landing-page (mvp)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
+
 export default function App() {
     return (
-        <>
-        </>
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
+        </Routes>
     );
 }
