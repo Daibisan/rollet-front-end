259cb36f2d1b7e8782b350d3649c5bfe6d0f4758
Daibisan <dibizeno@gmail.com>
2025-12-22 19:37:13 +0700

    minor padding change

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,12 +1,12 @@
 import RolletLogo from "@/components/ui/RolletLogo.jsx";
-import AppVersion from "../../components/ui/AppVersion";
+import AppVersion from "@/components/ui/AppVersion";
 import ContentLayout from "./components/ContentLayout";
 import Cloud from "@/components/ui/Cloud";
 
 export default function index({ layoutType }) {
     return (
         // body
-        <div className="relative flex h-full overflow-hidden px-16 py-6">
+        <div className="relative flex h-full overflow-hidden px-16 py-8">
             {/* wrapper */}
             <div className="flex w-full flex-col gap-22 font-[rubik-one]">
                 <header>
